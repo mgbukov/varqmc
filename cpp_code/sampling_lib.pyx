86ffd07f3fd5540629e943d168d5f0a469bfeab2
mgbukov <mgbukov@gmail.com>
2019-11-01 13:27:06 -0700

    one more check

--- cpp_code/sampling_lib.pyx
+++ cpp_code/sampling_lib.pyx
@@ -85,19 +85,20 @@ cdef extern from "<random>" namespace "std":
 '''
 
 
-cdef extern from "<boost/random/mersenne_twister.hpp>":
+cdef extern from "boost/random/mersenne_twister.hpp" namespace "boost::random" nogil:
     cdef cppclass mt19937 nogil:
         mt19937() nogil # we need to define this constructor to stack allocate classes in Cython
         mt19937(unsigned int seed) nogil # not worrying about matching the exact int type for seed
 
-cdef extern from "<boost/random/uniform_int_distribution.hpp>":
+cdef extern from "boost/random/uniform_int_distribution.hpp" namespace "boost::random" nogil:
 ##cdef extern from "<random>" namespace "std":
     cdef cppclass uniform_int_distribution[T] nogil:
         uniform_int_distribution() nogil
         uniform_int_distribution(T a, T b) nogil
         T operator()(mt19937 gen) nogil # ignore the possibility of using other classes for "gen"
 
-cdef extern from "<random>" namespace "std":
+cdef extern from "boost/random/uniform_real_distribution.hpp" namespace "boost::random" nogil:
+## cdef extern from "<random>" namespace "std":
     cdef cppclass uniform_real_distribution[T] nogil:
         uniform_real_distribution() nogil
         uniform_real_distribution(T a, T b) nogil
