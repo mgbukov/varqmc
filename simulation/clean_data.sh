6f20d60719721003a340eff15bee87cbe08acb66
mgbukov <mgbukov@gmail.com>
2019-12-03 12:01:25 -0500

    fixing job script

---
+++ simulation/clean_data.sh
@@ -0,0 +1,2 @@
+rm -f data/log_files/*
+rm -f data/data_files/*
--- simulation/submit.sh
+++ simulation/submit.sh
@@ -1,18 +1,23 @@
 #!bin/bash
+let "N_mpi=12"
+let "N_omp=1"
+let "N_tot=12"
 
 echo "#!/bin/bash -login" >> submission.sh
 echo "#$ -P f-dmrg" >> submission.sh
 echo "#$ -N job_VMC" >> submission.sh # Specify parameters in the job name. Don't specify the labels for k and SGE_TASK_ID 
-echo "#$ -l h_rt=12:00:00" >> submission.sh
-echo "#$ -pe omp 8" >> submission.sh # more processors
-echo "#$ -pe mpi_4_tasks_per_node 40" >> submission.sh
-#echo "#$ -l mem_per_core=16G" >> submission.sh # memory
+echo "#$ -l h_rt=2:00:00" >> submission.sh
+
+echo "#$ -pe omp ${N_omp}" >> submission.sh # more processors
+
+echo "#$ -pe mpi_4_tasks_per_node ${N_tot}" >> submission.sh
+
+#echo "#$ -l mem_per_core=12G" >> submission.sh # memory
 echo "#$ -m n" >> submission.sh
 
-#echo mpirun -np $NSLOTS /projectnb/f-dmrg/mbukov/.conda/envs/jax/bin/python main.py >> submission.sh
-echo mpirun -np $NSLOTS ~/.conda/envs/jax-noGPU/bin/python main.py >> submission.sh
 
-
+#echo mpirun -np ${N_mpi} /projectnb/f-dmrg/mbukov/.conda/envs/jax/bin/python main.py >> submission.sh
+echo mpirun -np ${N_mpi} ~/.conda/envs/jax-noGPU/bin/python ./main.py >> submission.sh
 
 qsub submission.sh
 
