3ce974f316f7ecf56ef3fedb358f293dd8487ff1
mgbukov <mgbukov@gmail.com>
2020-08-10 19:23:53 -0700

    fixing bugs

--- simulation/main.py
+++ simulation/main.py
@@ -20,7 +20,7 @@ import yaml
 # sacct -j "" --format="JobID,MaxRSS,AveRSS,MaxRSSNode"
 
 # request interactive node
-# salloc -N 32 -t 240 -C haswell -q interactive
+# salloc -N 32 -t 120 -C haswell -q interactive
 
 # qrsh -now n -l h_rt=12:00:00 -pe mpi_28_tasks_per_node 280
 
